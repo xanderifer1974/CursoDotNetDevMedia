0791eb8e4663869ce376de3eb54bb65b059b2edb
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-03 09:48:36 -0300

    Calculando a largura e profundidade

---
+++ CalculadoraPintura/CalculadoraPintura/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculadoraPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a largura do cômodo?");
+            double largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a profundidade do cômodo?");
+            double profundidade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área das paredes é:");
+            Console.WriteLine(2 * (largura + profundidade)* 2.9);
+
+            Console.WriteLine("A área do teto é:");
+            Console.WriteLine(largura * profundidade);
+
+            Console.ReadKey();
+        }
+    }
+}
