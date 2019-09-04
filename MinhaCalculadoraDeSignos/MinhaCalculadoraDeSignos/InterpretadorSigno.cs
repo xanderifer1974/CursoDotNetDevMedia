ea7cd299110cadaeedf3fc05c2ffb6c6c62a3ac7
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-04 08:35:57 -0300

    Criacao da classe Interpretador de Signo

---
+++ MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/InterpretadorSigno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaCalculadoraDeSignos
+{
+    class InterpretadorSigno
+    {
+        public Signo Interpretar(int dia, int mes)
+        {
+            Signo signo = null;
+            return signo;
+        }
+    }
+}
