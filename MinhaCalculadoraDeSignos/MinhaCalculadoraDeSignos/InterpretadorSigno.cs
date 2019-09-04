c4abb0df90cb0c635ee2bef6029907914ca2de16
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-04 12:33:12 -0300

    Criando Estrutura Condicional para  selecionar um signno

--- MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/InterpretadorSigno.cs
+++ MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/InterpretadorSigno.cs
@@ -11,6 +11,12 @@ namespace MinhaCalculadoraDeSignos
         public Signo Interpretar(int dia, int mes)
         {
             Signo signo = null;
+            if((dia >= 19 && mes == 2) || (dia <= 20 && mes == 3))
+            {
+
+                signo = new Signo();
+                signo.nome = "Peixes";
+            }
             return signo;
         }
     }
