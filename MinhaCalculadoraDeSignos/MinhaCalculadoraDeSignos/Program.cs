05d68c67118989e82743f7f5e3b3928bd41b5753
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-04 14:11:59 -0300

    Desenvolvendo o código na classe program, para interagir com as classes

--- MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/Program.cs
+++ MinhaCalculadoraDeSignos/MinhaCalculadoraDeSignos/Program.cs
@@ -10,7 +10,44 @@ namespace MinhaCalculadoraDeSignos
     {
         static void Main(string[] args)
         {
-            
+            string dia;
+            string mes;
+
+            Console.WriteLine("Em que dia voce nasceu?");
+            dia = Console.ReadLine();
+
+            Console.WriteLine("Em que mes voce nasceu?");
+            mes = Console.ReadLine();
+
+            int diaInt = 0;
+            int mesInt = 0;
+
+            try
+            {
+                diaInt = Convert.ToInt32(dia);
+                mesInt = Convert.ToInt32(mes);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+
+            InterpretadorSigno interpretador = new InterpretadorSigno();
+            Signo signo = interpretador.Interpretar(diaInt, mesInt);
+
+            if (signo != null)
+            {
+                Console.WriteLine(signo.nome + ": " + signo.caracteristicas);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Nao foi possivel interpretar seu signo");
+                Console.ReadLine();
+            }
         }
     }
 }
