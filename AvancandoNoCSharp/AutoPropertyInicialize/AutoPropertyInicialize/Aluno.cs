44a5c9ec92780d17b45148132e0057e5d38a3e0b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-26 16:32:29 -0300

    Auto Property Inicialize

---
+++ AvancandoNoCSharp/AutoPropertyInicialize/AutoPropertyInicialize/Aluno.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Aluno
+{
+    public static int Matricula { get; set; } = 0;
+    public string Nome { get; set; } = "Sem nome";
+    public string Status { get; set; } = Matricula == 0 ? "Não Matriculado" : "Matriculado";
+}
---
+++ AvancandoNoCSharp/AutoPropertyInicialize/AutoPropertyInicialize/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace AutoPropertyInicialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gp = new Aluno()
+            {
+                
+                Nome = "Natan Spinelli Ferreira"
+            };
+                       
+
+            WriteLine($"O Aluno {gp.Nome}, matrícula - {Aluno.Matricula}, foi aprovado com sucesso!!! >> {gp.Status}");
+            ReadLine();
+        }
+    }
+}
