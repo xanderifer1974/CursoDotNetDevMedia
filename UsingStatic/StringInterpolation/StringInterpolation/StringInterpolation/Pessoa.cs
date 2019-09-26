a5691844175f2e3e49415b59a898d22b9ddbedc3
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-09-26 14:49:45 -0300

    String Interpolation

---
+++ UsingStatic/StringInterpolation/StringInterpolation/StringInterpolation/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace StringInterpolation
+{
+    class Pessoa
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
